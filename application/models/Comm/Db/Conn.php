d2ec829e374b981cf9333c3d6bea0ccb7e8548c3
lc <chen.li@shoufuyou.com>
2018-08-30 15:10:31 +0800

    yartest first

---
+++ application/models/Comm/Db/Conn.php
@@ -0,0 +1,14 @@
+<?php
+class Comm_Db_Conn 
+{
+	private static $pools = array(); //存储mysql连接，实现mysql_connect的单例
+	
+	public static function getDb($alias = NULL) {
+		if (is_null($alias)) {
+			$name = 'test1';
+			
+		}
+		$db = Comm_Db_Mysql();
+	
+	}
+}
